057cddeba0632aba71baabcad4b34d44a98562e5
Marlom-Raul <79466275+Marlon3s@users.noreply.github.com>
2022-04-05 20:30:28 -0300

    Formatting

--- Schema-history/buscafeSchema.sql
+++ Schema-history/buscafeSchema.sql
@@ -1,34 +1,36 @@
 create database buscafe;
 use buscafe;
 
+#DISUSE
 create table tbl_tipoUsuario(
-	id int auto_increment primary key,
-    tipo varchar(15) not null,
-    descricao text
+	id 	      int auto_increment primary key,
+    	tipo          varchar(15) not null,
+    	descricao     text
 );
 
 insert into tbl_tipoUsuario(tipo, descricao) VALUES 
 	("Comum", "Usuario padrao"),
-    ("Corporativo", "Usuario com fim corporativo");
+    	("Corporativo", "Usuario com fim corporativo");
 
+#DISUSE
 create table tbl_descricaoEstatus(
-	id int auto_increment primary key,
-	descricao text
+	id int 	      auto_increment primary key,
+	descricao     text
 );
 
 insert into tbl_descricaoEstatus(descricao) VALUES 
 	("Desativada"),
-    ("Ativa"),
-    ("Suspensa");
+    	("Ativa"),
+	("Suspensa");
 
 create table tbl_usuario(
 	id            int auto_increment primary key,
-    nome          varchar(50) not null,
-    email         varchar(50) not null unique,
-    senha         varchar(50) not null,
+    	nome          varchar(50) not null,
+    	email         varchar(50) not null unique,
+    	senha         varchar(50) not null,
 	location      varchar(12) not null,
-    dtCria        datetime not null,
-    FK_id_uTipo   int,
+    	dtCria        datetime not null,
+    	FK_id_uTipo   int,
 	FK_uStatus    int,
     
     CONSTRAINT FK_id_uTipo FOREIGN KEY(FK_id_uTipo) REFERENCES tbl_tipoUsuario(id),
@@ -37,27 +39,30 @@ create table tbl_usuario(
 
 INSERT INTO tbl_usuario(nome, email, senha, dtCria, FK_id_uTipo, FK_uStatus) VALUES
 	("Marlom"  , "marlinho.123@gmail.com", md5('123456') , current_timestamp() , 1 , 2 ),
-    ("Igor"    , "igor.123@gmail.com"    , md5('123456') , current_timestamp() , 1 , 2 ),
-    ("Luis"    , "luis.123@gmail.com"    , md5('123456') , current_timestamp() , 1 , 2 ),
-    ("Gabriel" , "gabriel.123@gmail.com" , md5('123456') , current_timestamp() , 2 , 2 ),
-    ("Kaike"   , "kaike.123@gmail.com"   , md5('123456') , current_timestamp() , 2 , 2 ),
-    ("Jhonata" , "jhonata.123@gmail.com" , md5('123456') , current_timestamp() , 2 , 2 ),
-    ("Pedro"   , "pedro.123@gmail.com"   , md5('123456') , current_timestamp() , 2 , 2 );
-    select * from tbl_usuario;
+    	("Igor"    , "igor.123@gmail.com"    , md5('123456') , current_timestamp() , 1 , 2 ),
+    	("Luis"    , "luis.123@gmail.com"    , md5('123456') , current_timestamp() , 1 , 2 ),
+    	("Gabriel" , "gabriel.123@gmail.com" , md5('123456') , current_timestamp() , 2 , 2 ),
+    	("Kaike"   , "kaike.123@gmail.com"   , md5('123456') , current_timestamp() , 2 , 2 ),
+    	("Jhonata" , "jhonata.123@gmail.com" , md5('123456') , current_timestamp() , 2 , 2 ),
+    	("Pedro"   , "pedro.123@gmail.com"   , md5('123456') , current_timestamp() , 2 , 2 );
+
+SELECT * FROM tbl_usuario;
     
 ALTER TABLE tbl_usuario ADD COLUMN location varchar(15) not null after senha,
 						ADD COLUMN location varchar(15) not null after senha;
 UPDATE tbl_usuario SET location = '45.174.181.242' WHERE id = 2;
 UPDATE tbl_usuario SET location = '192.168.100.2' WHERE id = 4;
 
+#DISUSE
 create table tbl_apontamento(
-	id int auto_increment primary key,
-    idUsuario int,
-    nomeUsuario varchar(50) not null,
-    dtAlter datetime,
-    alterDescri varchar(100),
-    CONSTRAINT idUsuario FOREIGN KEY (idUsuario) REFERENCES tbl_usuario(id)
-);
+	id            int auto_increment primary key,
+    	idUsuario     int,
+    	nomeUsuario   varchar(50) not null,
+    	dtAlter       datetime,
+    	alterDescri   varchar(100),
+	
+    	CONSTRAINT idUsuario FOREIGN KEY (idUsuario) REFERENCES tbl_usuario(id)
+);	
 
 /*
 É necessário fazer uma alteração na tabela usuario,
